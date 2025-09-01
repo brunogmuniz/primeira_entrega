d9f5f0e41dda47c76ed6c8888731e9a6789e05b8
Brunogmuniz <devbrunomuniz@gmail.com>
2025-09-01 17:58:21 -0300

    primeiro commit

---
+++ src/main/java/br/csi/trilhagaucha/TrilhaGauchaApplication.java
@@ -0,0 +1,13 @@
+package br.csi.trilhagaucha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrilhaGauchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrilhaGauchaApplication.class, args);
+	}
+
+}
